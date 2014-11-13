e83dc102fe9fa93b2be8512f1c488da26bcc5779
Tamanh <tamvn@uw.edu>
2014-11-13 09:51:00 -0800

    Applied active style class

--- js/app.js
+++ js/app.js
@@ -12,4 +12,8 @@ angular.module('AddressBook', [])
     .controller('AddressController', function($scope) {
         $scope.employees = pawneeEmployees;
         $scope.order = 'lastName';
+
+        $scope.isSortedBy = function(colName) {
+        	return $scope.order == colName;
+        };
     });
